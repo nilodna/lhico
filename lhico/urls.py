d5ea11993d97340cf705caeb54ba2eb49ff63d5f
Danilo A Silva <nilodna@gmail.com>
2015-09-13 21:49:26 -0300

    change urls file to core

--- lhico/urls.py
+++ lhico/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'website.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'', include('core.urls')),
+   )
